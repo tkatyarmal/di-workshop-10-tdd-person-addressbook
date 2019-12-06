6604bfb2f5196d56b8b31946fddbe70ab9e6bd6f
Tejas Katyarmal <tkatyarmal@uk-c02tvca4htd6.uk.deloitte.com>
2019-12-06 14:03:16 +0000

    pushing everything up

--- models/person.js
+++ models/person.js
@@ -1,1 +1,10 @@
-// Your Person class goes here!
+class Person {
+    constructor(firstName, surname, dob) {
+        this.firstName = firstName.charAt(0).toUpperCase() + firstName.slice(1)
+        this.surname = surname
+        this.dob = dob
+    }
+
+}
+
+module.exports = Person
--- test/person.test.js
+++ test/person.test.js
@@ -1,4 +1,29 @@
 const chai = require('chai')
 const expect = chai.expect
 
-// Your Person test goes here!
+const Person = require('../models/person.js')
+
+describe('Person', () => {
+    it('should initialize properly', () => {
+        var person = new Person('Joe', 'Bloggs', '1 Jan 1990')
+
+        expect(person.firstName).to.equal('Joe')
+        expect(person.surname).to.equal('Bloggs')
+        expect(person.dob).to.equal('1 Jan 1990')
+    })
+
+    it('should capitalize the first name', () => {
+        var person = new Person('amy', 'Nother', '1 Jan 1990')
+
+        expect(person.firstName).to.equal('Amy')
+
+    })
+
+    it('should return a full name', () => {
+        var person = new Person('Joe', 'Bloggs', '1 Jan 1990')
+
+        expect(person.fullName()).to.equal('Joe Bloggs')
+
+    })
+
+})
